48a713d728592a145aeac209e1dc7da2987fd307
Thomas Dullien <thomasdullien@thomasdullien.zrh.corp.google.com>
2017-10-16 13:42:04 +0200

    Moved data generation to /var/tmp from /tmp

--- testdata/generate_test_data.sh
+++ testdata/generate_test_data.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-rm -rf /tmp/train/
-mkdir /tmp/train
+work_directory=/var/tmp/train
+
+rm -rf $work_directory
+mkdir $work_directory
 
 # Process the GCC-compiled files.
 for inputfile in $(ls ../testdata/unrar.5.5.3.builds/unrar.x??.O?); do
   shasum=$(sha256sum $inputfile);
   id=${shasum:0:16};
   filename=$(echo $inputfile | rev | cut -d"/" -f1 | rev);
-  ../bin/functionfingerprints ELF $inputfile 5 true >> /tmp/train/functions_$id.txt;
+  ../bin/functionfingerprints ELF $inputfile 5 true >> $work_directory/functions_$id.txt;
   objdump -t $inputfile | grep " g " | grep \.text | sort -k6 | cut -d" " -f1,22- | while read -r syms; do
     address=$(echo $syms | cut -d" " -f1);
     symbolname=$(echo $syms | cut -d" " -f2 | tr -d '\n');
     # Pad address to 16 characters.
     while [ ${#address} -lt 16 ]; do address=0$address; done;
     echo $id $inputfile $address $(echo "$symbolname" | base64 -w0) false;
-  done | sort -k3> /tmp/train/symbols_$id.txt;
-  cat /tmp/train/functions_$id.txt | cut -d" " -f1 | tr ":" " " | sort -k2 > /tmp/train/functions_stripped_$id.txt;
-  join -1 2 -2 3 -o 2.1,2.2,2.3,2.4,2.5 /tmp/train/functions_stripped_$id.txt /tmp/train/symbols_$id.txt | sort -k4 > /tmp/train/extracted_symbols_$id.txt;
+  done | sort -k3> $work_directory/symbols_$id.txt;
+  cat $work_directory/functions_$id.txt | cut -d" " -f1 | tr ":" " " | sort -k2 > $work_directory/functions_stripped_$id.txt;
+  join -1 2 -2 3 -o 2.1,2.2,2.3,2.4,2.5 $work_directory/functions_stripped_$id.txt $work_directory/symbols_$id.txt | sort -k4 > $work_directory/extracted_symbols_$id.txt;
 done
 
 # Process the VS2015-compiled files. For these, we can't use objdump, and we
@@ -27,7 +29,7 @@ for inputfile in $(find ../testdata/unrar.5.5.3.builds/VS2015/ -iname \*.exe); d
   shasum=$(sha256sum $inputfile);
   id=${shasum:0:16};
   filename=$(echo $inputfile | rev | cut -d"/" -f1 | rev);
-  ../bin/functionfingerprints PE $inputfile 5 true >> /tmp/train/functions_$id.txt;
+  ../bin/functionfingerprints PE $inputfile 5 true >> $work_directory/functions_$id.txt;
   # Get the default image base. For 32-bit executables, this is 0x400000, for
   # 64-bit executables, it is 0x140000000.
   filetype=$(file -b $inputfile);
@@ -46,14 +48,14 @@ for inputfile in $(find ../testdata/unrar.5.5.3.builds/VS2015/ -iname \*.exe); d
     address=$((0x$address + $defaultbase));
     address=$(printf "%16.16lx\n" $address);
     [[ ! -z $symbol ]] && echo $id $inputfile $address $(echo "$symbol" | base64 -w0) false;
-  done | sort -k3> /tmp/train/symbols_$id.txt;
-  cat /tmp/train/functions_$id.txt | cut -d" " -f1 | tr ":" " " | sort -k2 > /tmp/train/functions_stripped_$id.txt;
-  join -1 2 -2 3 -o 2.1,2.2,2.3,2.4,2.5 /tmp/train/functions_stripped_$id.txt /tmp/train/symbols_$id.txt | sort -k4 > /tmp/train/extracted_symbols_$id.txt;
+  done | sort -k3> $work_directory/symbols_$id.txt;
+  cat $work_directory/functions_$id.txt | cut -d" " -f1 | tr ":" " " | sort -k2 > $work_directory/functions_stripped_$id.txt;
+  join -1 2 -2 3 -o 2.1,2.2,2.3,2.4,2.5 $work_directory/functions_stripped_$id.txt $work_directory/symbols_$id.txt | sort -k4 > $work_directory/extracted_symbols_$id.txt;
 done
 
 # That hurt. We now have metadata files. We want to form known-matching pairs.
-for inputfile in $(ls /tmp/train/extracted_symbols_*.txt); do
-  for inputfile2 in $(ls /tmp/train/extracted_symbols_*.txt); do
+for inputfile in $(ls $work_directory/extracted_symbols_*.txt); do
+  for inputfile2 in $(ls $work_directory/extracted_symbols_*.txt); do
     if [[ "$inputfile2" > "$inputfile" ]]
     then
       join -1 4 -2 4 -o 1.1,1.3,2.1,2.3 $inputfile $inputfile2 | while read -r line; do
@@ -62,40 +64,40 @@ for inputfile in $(ls /tmp/train/extracted_symbols_*.txt); do
       done;
     fi
   done;
-done >> /tmp/train/attract.txt
+done >> $work_directory/attract.txt
 
 # We need to split the data into a training and validation set. Select 100
 # random pairs of functions, and the functions in them off into a different
 # attract.txt file.
 
-mkdir /tmp/train/training_data;
-mkdir /tmp/train/validation_data;
-cp /tmp/train/attract.txt /tmp/train/attract_temp.txt;
-for func in $(cat /tmp/train/attract.txt | sort -R | head -n 50); do
+mkdir $work_directory/training_data;
+mkdir $work_directory/validation_data;
+cp $work_directory/attract.txt $work_directory/attract_temp.txt;
+for func in $(cat $work_directory/attract.txt | sort -R | head -n 50); do
   # Filter the function in question out of the attract.txt.
-  cat /tmp/train/attract_temp.txt | grep -v "$func" > /tmp/train/attract_temp2.txt;
+  cat $work_directory/attract_temp.txt | grep -v "$func" > $work_directory/attract_temp2.txt;
   # Place it into the relevant evaluation file.
-  cat /tmp/train/attract_temp.txt | grep "$i" >> /tmp/train/validation_data/attract.txt;
+  cat $work_directory/attract_temp.txt | grep "$i" >> $work_directory/validation_data/attract.txt;
   # Overwrite the temporary file (we filter iteratively)
-  cp /tmp/train/attract_temp2.txt /tmp/train/attract_temp.txt;
+  cp $work_directory/attract_temp2.txt $work_directory/attract_temp.txt;
 done
-cp /tmp/train/attract_temp.txt /tmp/train/training_data/attract.txt;
-cat /tmp/train/validation_data/attract.txt | sort | uniq > /tmp/train/attract_temp.txt;
-cp /tmp/train/attract_temp.txt /tmp/train/validation_data/attract.txt;
+cp $work_directory/attract_temp.txt $work_directory/training_data/attract.txt;
+cat $work_directory/validation_data/attract.txt | sort | uniq > $work_directory/attract_temp.txt;
+cp $work_directory/attract_temp.txt $work_directory/validation_data/attract.txt;
 
 # To generate non-matching pairs, we simply shuffle the matching pairs.
-cat /tmp/train/training_data/attract.txt | cut -d" " -f1 | sort -R > /tmp/train/repulse_temp.txt;
-cat /tmp/train/training_data/attract.txt | cut -d" " -f2 | sort -R > /tmp/train/repulse_temp2.txt;
-paste -d" " /tmp/train/repulse_temp.txt /tmp/train/repulse_temp2.txt > /tmp/train/training_data/repulse.txt;
+cat $work_directory/training_data/attract.txt | cut -d" " -f1 | sort -R > $work_directory/repulse_temp.txt;
+cat $work_directory/training_data/attract.txt | cut -d" " -f2 | sort -R > $work_directory/repulse_temp2.txt;
+paste -d" " $work_directory/repulse_temp.txt $work_directory/repulse_temp2.txt > $work_directory/training_data/repulse.txt;
 
 # Do the same for the validation data.
-cat /tmp/train/validation_data/attract.txt | cut -d" " -f1 | sort -R > /tmp/train/repulse_temp.txt;
-cat /tmp/train/validation_data/attract.txt | cut -d" " -f2 | sort -R > /tmp/train/repulse_temp2.txt;
-paste -d" " /tmp/train/repulse_temp.txt /tmp/train/repulse_temp2.txt > /tmp/train/validation_data/repulse.txt;
+cat $work_directory/validation_data/attract.txt | cut -d" " -f1 | sort -R > $work_directory/repulse_temp.txt;
+cat $work_directory/validation_data/attract.txt | cut -d" " -f2 | sort -R > $work_directory/repulse_temp2.txt;
+paste -d" " $work_directory/repulse_temp.txt $work_directory/repulse_temp2.txt > $work_directory/validation_data/repulse.txt;
 
 # Create functions.txt
-cat /tmp/train/functions_????????????????.txt > /tmp/train/training_data/functions.txt
-cat /tmp/train/functions_????????????????.txt > /tmp/train/validation_data/functions.txt
+cat $work_directory/functions_????????????????.txt > $work_directory/training_data/functions.txt
+cat $work_directory/functions_????????????????.txt > $work_directory/validation_data/functions.txt
 
 # Ok, we are done.
 
