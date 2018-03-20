0ad59ac25bf654f95d29e010c4476fa34d548621
thomasdullien <thomasdullien@thomasdullien.roam.corp.google.com>
2018-03-20 21:58:55 +0100

    Added missing test file

---
+++ mappedtextfile_test.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "mappedtextfile.hpp"
+
+TEST(mappedtextfile, count_lines) {
+  MappedTextFile mapped("../testdata/training/functions.txt");
+  auto lines = mapped.GetLineIterator();
+  int line_count = 0;
+  while (lines.HasMore()) {
+    ++line_count;
+    ++lines;
+  }
+  EXPECT_EQ(line_count, 166);
+}
+
+TEST(mappedtextfile, count_words) {
+  MappedTextFile mapped("../testdata/training/functions.txt");
+  auto lines = mapped.GetLineIterator();
+  int word_count = 0;
+  while (lines.HasMore()) {
+    auto words = mapped.GetWordIterator(lines);
+    while (words.HasMore()) {
+      const char *wordstart, *wordend;
+      words.Get(&wordstart, &wordend);
+      std::string word(wordstart, wordend);
+      ++words;
+      ++word_count;
+    }
+    ++lines;
+  }
+  EXPECT_EQ(word_count, 49411);
+}
+
+
