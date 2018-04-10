62caa3dc33996981bd11ca9549e61b1da37076e3
Thomas Dullien <thomasdullien@thomasdullien.zrh.corp.google.com>
2018-04-10 15:19:05 +0200

    Added code to dump features to /var/tmp for easier diagnostics

---
+++ searchbackend/functionsimhashfeaturedump.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+#include "disassembly/flowgraph.hpp"
+#include "disassembly/functionfeaturegenerator.hpp"
+#include "searchbackend/functionsimhashfeaturedump.hpp"
+
+// Writes a DOT file for a given graphlet.
+void WriteFeatureDictionaryEntry(uint64_t hashA, uint64_t hashB,
+  const Flowgraph& graphlet) {
+  char buf[256];
+  sprintf(buf, "/var/tmp/%16.16lx%16.16lx.dot", hashA, hashB);
+  graphlet.WriteDot(std::string(buf));
+}
+
+// Writes a JSON file for a given MnemTuple.
+void WriteFeatureDictionaryEntry(uint64_t hashA, uint64_t hashB, 
+  const MnemTuple& tuple) {
+  char buf[256];
+  sprintf(buf, "/var/tmp/%16.16lx%16.16lx.json", hashA, hashB);
+  std::ofstream jsonfile;
+  jsonfile.open(std::string(buf));
+  jsonfile << "[ " << std::get<0>(tuple) << ", " << std::get<1>(tuple) << ", "
+    << std::get<2>(tuple) << " ]" << std::endl;
+}
+
---
+++ searchbackend/functionsimhashfeaturedump.hpp
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONSIMHASHFEATUREDUMP_HPP
+#define FUNCTIONSIMHASHFEATUREDUMP_HPP
+
+#include "disassembly/functionfeaturegenerator.hpp"
+#include "disassembly/flowgraph.hpp"
+
+// Writes a DOT file for a given graphlet.
+void WriteFeatureDictionaryEntry(uint64_t hashA, uint64_t hashB,
+  const Flowgraph& graphlet);
+
+// Writes a JSON file for a given MnemTuple.
+void WriteFeatureDictionaryEntry(uint64_t hashA, uint64_t hashB, 
+  const MnemTuple& tuple);
+
+#endif // FUNCTIONSIMHASHFEATUREDUMP_HPP
