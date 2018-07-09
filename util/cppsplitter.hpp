64870ebc41383b4ac42072811999b157c78150e9
thomasdullien <thomasdullien@thomasdullien.roam.corp.google.com>
2018-07-09 06:33:49 +0200

    Fixed issues with the symbol stemming during data generation

---
+++ util/cppsplitter.hpp
@@ -0,0 +1,8 @@
+#ifndef CPPSPLITTER_HPP
+#define CPPSPLITTER_HPP
+#include <string>
+#include <deque>
+
+void CppSplitter(const std::string& input, std::deque<std::string>& result);
+
+#endif // CPPSPLITTER_HPP
