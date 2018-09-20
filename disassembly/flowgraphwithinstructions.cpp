1afdff7b6b6242ee33c0e71245ffb189e1ad1ac5
thomasdullien <thomasdullien@thomasdullien.roam.corp.google.com>
2018-09-20 08:46:30 +0200

    Added single-distance graphlets, too (matching accuracy gets deterioated too much for tiny changes otherwise)

--- disassembly/flowgraphwithinstructions.cpp
+++ disassembly/flowgraphwithinstructions.cpp
@@ -79,6 +79,9 @@ FlowgraphWithInstructionsFeatureGenerator::FlowgraphWithInstructionsFeatureGener
   std::vector<address> nodes;
   flowgraph_.GetNodes(&nodes);
   for (const auto& node : nodes) {
+    nodes_and_distance_.push(std::make_pair(node, 1));
+  }
+  for (const auto& node : nodes) {
     nodes_and_distance_.push(std::make_pair(node, 2));
   }
   for (const auto& node : nodes) {
