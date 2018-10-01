74e4ee060012c467e9e3571afe2021e84941baa7
Thomas Dullien <thomasdullien@thomasdullien.zrh.corp.google.com>
2018-10-01 16:31:12 +0200

    Fixed some of the ultra-brittle tests.

--- disassembly/flowgraphwithinstructions_test.cpp
+++ disassembly/flowgraphwithinstructions_test.cpp
@@ -69,8 +69,8 @@ TEST(flowgraphwithinstructions, creategraph) {
       &feature_hashes);
     uint64_t hash1 = hashes[0];
     uint64_t hash2 = hashes[1];
-    EXPECT_EQ(hash1, 0x9c5b458691685f5e);
-    EXPECT_EQ(hash2, 0x76a3474775cba64e);
+    EXPECT_EQ(hash1, 0xACEB07449170DFCF);
+    EXPECT_EQ(hash2, 0x56df46c771e9a4df);
   }
   // Check fg_dyninst
   {
@@ -90,8 +90,23 @@ TEST(flowgraphwithinstructions, creategraph) {
 }
 
 TEST(flowgraphwithinstructions, parsejson) {
-  const char* json_string = 
+  const char* json_string =
   R"json({"edges":[{"destination":1518838580,"source":1518838565},{"destination":1518838572,"source":1518838565},{"destination":1518838578,"source":1518838572},{"destination":1518838574,"source":1518838572},{"destination":1518838580,"source":1518838574},{"destination":1518838578,"source":1518838574},{"destination":1518838580,"source":1518838578}],"name":"CFG","nodes":[{"address":1518838565,"instructions":[{"mnemonic":"xor","operands":["EAX","EAX"]},{"mnemonic":"cmp","operands":["[ECX + 4]","EAX"]},{"mnemonic":"jnle","operands":["5a87a334"]}]},{"address":1518838572,"instructions":[{"mnemonic":"jl","operands":["5a87a332"]}]},{"address":1518838574,"instructions":[{"mnemonic":"cmp","operands":["[ECX]","EAX"]},{"mnemonic":"jnb","operands":["5a87a334"]}]},{"address":1518838578,"instructions":[{"mnemonic":"mov","operands":["AL","1"]}]},{"address":1518838580,"instructions":[{"mnemonic":"ret near","operands":["[ESP]"]}]}]})json";
+
+  std::string expected_disassembly =
+    "\n[!] Function at 5a87a325\t\tBlock at 5a87a325 (3)\n"
+    "\t\t\t xor EAX, EAX\n"
+    "\t\t\t cmp [ECX + 4], EAX\n"
+    "\t\t\t jnle 5a87a334\n"
+    "\t\tBlock at 5a87a32c (1)\n"
+    "\t\t\t jl 5a87a332\n"
+    "\t\tBlock at 5a87a32e (2)\n"
+    "\t\t\t cmp [ECX], EAX\n"
+    "\t\t\t jnb 5a87a334\n"
+    "\t\tBlock at 5a87a332 (1)\n"
+    "\t\t\t mov AL, 1\n"
+    "\t\tBlock at 5a87a334 (1)\n"
+    "\t\t\t ret near [ESP]\n";
 
   std::unique_ptr<FlowgraphWithInstructions> ptr(new FlowgraphWithInstructions());
   FlowgraphWithInstructions* temp = ptr.get();
@@ -100,10 +115,6 @@ TEST(flowgraphwithinstructions, parsejson) {
   EXPECT_TRUE(ptr->HasNode(1518838565));
   EXPECT_TRUE(ptr->HasNode(1518838572));
 
-  FunctionSimHasher hasher("", false);
-  FlowgraphWithInstructionsFeatureGenerator feature_gen(*ptr);
-  std::vector<uint64_t> output_hashes;
-  hasher.CalculateFunctionSimHash(&feature_gen, 128, &output_hashes);
-
-  EXPECT_EQ(output_hashes[0], 0xa7ef296fa5dea3ee);
+  std::string disasm = temp->GetDisassembly();
+  EXPECT_EQ(disasm, expected_disassembly);
 }
